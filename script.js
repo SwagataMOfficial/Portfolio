e3fb7557dfa2307a17e58a7bb1ebb0a4ee1ceba2
Swagata Mukherjee <138999457+SwagataMOfficial@users.noreply.github.com>
2023-07-13 21:24:29 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const search = document.getElementById('search-btn');
-
-// adding searching functionality to all the navigation bars of the website.
-search.addEventListener('click', ()=>{
-    let search_text = document.getElementById('search').value;
-    alert("You Searched : " + search_text);
-    document.getElementById('search').value = "";
-});
